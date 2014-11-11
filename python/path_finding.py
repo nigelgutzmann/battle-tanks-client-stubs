299d1ecd1606e4eaa1eeab27fd230642a1d6fac7
Nigel Gutzmann <nigel_2013@thenext36.ca>
2014-11-10 21:26:09 -0700

    adf

--- python/path_finding.py
+++ python/path_finding.py
@@ -15,7 +15,7 @@ class PathFinder(object):
             return []
 
         frontier = PriorityQueue()
-        frontier.put((0, self.source,))
+        frontier.put(0, self.source)
         came_from = {}
         cost_so_far = {}
         came_from[self.source] = None
@@ -39,7 +39,7 @@ class PathFinder(object):
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
                     cost_so_far[next] = new_cost
                     priority = new_cost + self.heuristic(self.target, next)
-                    frontier.put((priority, next,))
+                    frontier.put(priority, next)
                     came_from[next] = current
 
         # now reconstruct the path
