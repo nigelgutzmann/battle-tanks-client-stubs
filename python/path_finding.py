868ab8774f9a8fbff2e4b2508bb8d513ce46e2ba
Nigel Gutzmann <nigel_2013@thenext36.ca>
2014-11-10 17:35:59 -0700

    forgot to save

--- python/path_finding.py
+++ python/path_finding.py
@@ -60,13 +60,13 @@ class PathFinder(object):
         ### TODO: check if there is a danger of being shot here, and adjust accordingly
         return 1
 
-    class Point(object):
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-        def __hash__(self):
-            return hash((self.x, self.y))
+    def __hash__(self):
+        return hash((self.x, self.y))
 
-        def __eq__(self, other):
-            return (self.x, self.y) == (other.x, other.y)
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
