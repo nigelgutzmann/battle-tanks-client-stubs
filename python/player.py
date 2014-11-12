2a51ae5f963124735fc47435012f09ebfe5f799e
Nigel Gutzmann <nigel_2013@thenext36.ca>
2014-11-12 15:27:51 -0700

    wooo

--- python/player.py
+++ python/player.py
@@ -16,8 +16,8 @@ class Player(object):
 
     def play_game(self, client_token):
         # TODO: later, we will want to spawn two threads here
-        decoded = False
-        while not decoded:
+        stop = False
+        while True:
             # get a message
             message = self.comm.receive(communication.Communication.Origin.PublishSocket)
             message = self.comm.receive(communication.Communication.Origin.PublishSocket)
@@ -39,4 +39,4 @@ class Player(object):
             # decide what to do
             if decoded == "GAMESTATE":
                 self.algorithm.make_move(client_token)
-                #decoded = True
+                #stop = True
