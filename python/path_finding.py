78bfee7afe55f1401dd822ddb835b99b881ff614
Nigel Gutzmann <nigel_2013@thenext36.ca>
2014-11-13 12:30:23 -0700

    over power everything

--- python/path_finding.py
+++ python/path_finding.py
@@ -87,7 +87,7 @@ class PathFinder(object):
         else:
             for projectile in self.projectiles:
                 if self.projectile_will_hit(next, projectile):
-                    return 100
+                    return 1000
         return 1
 
     def projectile_will_hit(self, point, projectile):
