d4b202591c565d24ca3b7835285c03f293e0959f
Nigel Gutzmann <nigel_2013@thenext36.ca>
2014-11-13 16:19:52 -0700

    less lead

--- python/algorithm.py
+++ python/algorithm.py
@@ -31,9 +31,9 @@ class Algorithm(object):
 
         # the first point is the point that we are currently at, so we have to take the second one in the list
         next_point = None
-        if len(route) > 4:
-            next_point = route[3]
-        elif len(route) > 3:
+        #if len(route) > 4:
+        #    next_point = route[3]
+        if len(route) > 3:
             next_point = route[2]
         elif len(route) > 2:
             next_point = route[1]
